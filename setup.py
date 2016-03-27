144b3f644189e95495488885fa46a212cb1401f6
jan <jan@orange.mpxd.net>
2016-03-27 16:17:27 -0700

    add float_raster as dependency'

--- setup.py
+++ setup.py
@@ -10,10 +10,11 @@ setup(name='gridlock',
       url='https://mpxd.net/gogs/jan/gridlock',
       packages=['gridlock'],
       install_requires=[
-            'numpy'
+            'numpy',
+            'float_raster',
       ],
       dependency_links=[
-            'git+https://mpxd.net/gogs/jan/float_raster.git@release'
+            'git+https://mpxd.net/gogs/jan/float_raster.git@release#egg=float_raster-0.1'
       ],
       extras_require={
           'visualization': ['matplotlib'],
